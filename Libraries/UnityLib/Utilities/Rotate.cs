392b0c3b96b5c1f166c308725c3a4f533fbfd638
purple-ben <ben.wager@purple.nl>
2020-02-04 09:50:43 +0100

    Added UIDrag and Rotate

---
+++ Libraries/UnityLib/Utilities/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+namespace UnityPureMVC.Core.Libraries.UnityLib.Utilities
+{
+    public class Rotate : MonoBehaviour
+    {
+        public Vector3 velocity;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(velocity);
+        }
+    }
+}
---
+++ Libraries/UnityLib/Utilities/UI/UIDrag.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UnityPureMVC.Core.Libraries.UnityLib.Utilities.UI
+{
+    public class UIDrag : MonoBehaviour, IBeginDragHandler, IDragHandler
+    {
+        public Transform elementToDrag;
+
+        private Vector2 offset = new Vector2();
+
+        private void Start()
+        {
+            if(elementToDrag == null)
+            {
+                elementToDrag = transform;
+            }
+        }
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            offset = new Vector2(elementToDrag.position.x, elementToDrag.position.y) - eventData.position;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            elementToDrag.position = eventData.position + offset;
+        }
+    }
+}
